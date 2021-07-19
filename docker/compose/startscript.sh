f7107bd60c79eb7fb165abb79039697e45e5e246
Sai Karthik <kskarthik@disroot.org>
2021-07-20 00:11:09 +0530

    pull pip dependencies for gkwebapp on container load/restart

--- docker/compose/startscript.sh
+++ docker/compose/startscript.sh
@@ -6,9 +6,16 @@ git pull
 echo "checking database status"
 python3 initdb.py
 
+echo "checking pip dependencies"
+python3 setup.py develop
+
 echo "pulling latest gkwebapp changes ..."
 cd /gnukhata/gkwebapp
 git pull
+
+echo "checking pip dependencies"
+python3 setup.py develop
+
 
 echo "starting supervisor"
 cd /gnukhata
