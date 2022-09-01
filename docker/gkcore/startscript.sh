95f3abe8ed38b957725c4bebcdc52cae4a0d4afc
Sai Karthik <kskarthik@disroot.org>
2022-09-01 15:26:18 +0530

    docker image for gkapp & folder layout changes

--- docker/gkcore/startscript.sh
+++ docker/gkcore/startscript.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# # delete virtualenv & recreate a fresh one
-# rm -rf gkenv
-# python3 -m venv gkenv
-# . gkenv/bin/activate
-
 #gkcore
 cd /gnukhata/gkcore
 echo "pulling latest gkcore changes ..."
@@ -15,18 +10,21 @@ python3 initdb.py
 echo "checking pip dependencies"
 pip3 install -r requirements.txt
 python3 setup.py develop
+pserve production.ini
 
 #gkwebapp
 cd /gnukhata/gkwebapp
 echo "pulling latest gkwebapp changes ..."
 git pull
-
 echo "checking pip dependencies"
 pip3 install -r requirements.txt
 python3 setup.py develop
+pserve production.ini
 
+# this is causing CORS issues in some API's
+# Disabling until furthur inspection.
 # supervisor
-echo "starting supervisor"
-cd /gnukhata
-supervisord -c supervisord.conf
+# echo "starting supervisor"
+# cd /gnukhata
+# supervisord -c supervisord.conf
 tail -f /dev/null
